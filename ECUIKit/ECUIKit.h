f066dd4b4fb04004e7db2e9e61aa5773a17746e4
Daniel Alm <Daniel.Alm@ForumD.net>
2013-07-09 12:53:48 +0200

    ECUIKit.h: Add some color helpers.

--- ECUIKit/ECUIKit.h
+++ ECUIKit/ECUIKit.h
@@ -9,12 +9,20 @@
 #import <Foundation/Foundation.h>
 
 #if TARGET_OS_IPHONE
+
+// iOS
 #define ECFont UIFont
 #define ECImage UIImage
 #define ECColor UIColor
 #define ECView UIView
 #define ECBezierPath UIBezierPath
+
+#define ECRGBCOLOR(r,g,b) [ECColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1]
+#define ECRGBACOLOR(r,g,b,a) [ECColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]
+
 #else
+
+// Mac
 #define ECFont NSFont
 #define ECImage NSImage
 #define ECColor NSColor
@@ -25,9 +33,11 @@
 NSGraphicsContext * nscg = [NSGraphicsContext graphicsContextWithGraphicsPort:ctx flipped:YES]; \
 [NSGraphicsContext setCurrentContext:nscg]; }
 #define UIGraphicsPopContext() [NSGraphicsContext restoreGraphicsState];
-#endif
 
-#define ECRGBCOLOR(r,g,b) [ECColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1]
+#define ECRGBCOLOR(r,g,b) [ECColor colorWithSRGBRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1]
+#define ECRGBACOLOR(r,g,b,a) [ECColor colorWithSRGBRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]
+
+#endif
 
 #import "ECLog.h"
 
