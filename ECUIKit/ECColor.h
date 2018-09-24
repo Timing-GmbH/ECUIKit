825cbbad2483fcac585f426a1b1b0fa3d480ecf4
Daniel Alm <Daniel.Alm@ForumD.net>
2018-09-24 17:20:11 +0200

    PC-319: More Dark Mode.

--- ECUIKit/ECColor.h
+++ ECUIKit/ECColor.h
@@ -14,7 +14,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIColor (ECUIKit)
 
+@property(class, nonatomic, readonly) UIColor *ec_systemRedColor;
+@property(class, nonatomic, readonly) UIColor *ec_systemBlueColor;
+
 @property(class, nonatomic, readonly) UIColor *ec_textColor;
+@property(class, nonatomic, readonly) UIColor *ec_secondaryLabelColor;
 
 @end
 
--- ECUIKit/ECColor.m
+++ ECUIKit/ECColor.m
@@ -10,11 +10,35 @@
 
 @implementation UIColor (ECUIKit)
 
++ (UIColor *)ec_systemRedColor {
+#if TARGET_OS_IPHONE
+	return [self redColor];
+#else
+	return [self systemRedColor];
+#endif
+}
+
++ (UIColor *)ec_systemBlueColor {
+#if TARGET_OS_IPHONE
+	return [self blueColor];
+#else
+	return [self systemBlueColor];
+#endif
+}
+
 + (UIColor *)ec_textColor {
 #if TARGET_OS_IPHONE
 	return [self blackColor];
 #else
 	return [self textColor];
+#endif
+}
+
++ (UIColor *)ec_secondaryLabelColor {
+#if TARGET_OS_IPHONE
+	return ECRGBACOLOR(0, 0, 0, 0.5);
+#else
+	return [self secondaryLabelColor];
 #endif
 }
 
